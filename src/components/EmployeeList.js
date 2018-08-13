b9a2c943d0e3c98dbf435bf2f95397e117ba847b
leonardolima <leonardo.bruno@outlook.com>
2018-08-13 11:48:18 -0300

    add list view feature

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,26 +1,66 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, Text, ListView } from 'react-native';
 import { employeesFetch } from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
     componentDidMount() {
         this.props.employeesFetch();
+        this.createDataSource(this.props);       
     }
-    
+
+    componentWillReceiveProps(nextProps) {
+        this.createDataSource(nextProps);
+    }
+
+    createDataSource({ employees }) {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.dataSource = ds.cloneWithRows(employees);
+    }
+
+    renderRow(employee) {
+        return <ListItem employee={employee} />;
+    }
+
+    renderListView() {
+        if (this.dataSource) {
+            return (
+                <ListView
+                    enableEmptySections
+                    dataSource={this.dataSource}
+                    renderRow={this.renderRow}
+                />
+            );
+        }
+
+        return <Text>loading...</Text>;
+    }
+
     render() {
+        console.log(this.props);
+        console.log(this.dataSource);
+        
         return (
             <View>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
+                {this.renderListView()}
             </View>
         );
     }
 }
 
-export default connect(null, { employeesFetch })(EmployeeList);
+const mapStateToProps = state => {
+    const employees = _.map(state.employees, (val, uid) => {
+        return { ...val, uid };
+    });
+
+    return {
+        employees
+    };
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
